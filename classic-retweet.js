81b9b525ae688eb013e2b957fa8ae56677542f4f
Joel McCoy <joelmccoy@gmail.com>
2013-04-11 15:12:54 -0400

    Enable native reply emulation

--- classic-retweet.js
+++ classic-retweet.js
@@ -24,54 +24,41 @@
     });
   }
   */
+  
   $("#page-container").delegate(".tweet", "mouseover", function() {
     if (!$(this).attr(enabled)) {
       $(this).attr(enabled, "true");
-      var replyAction = $(this).find(".action-reply-container").first(); // guard against embedded copy ("permalink-tweet" vs "original-tweet") on individual tweet page
-      var classicRetweetAction = replyAction.clone();
-      var link = classicRetweetAction.find(".js-action-reply");
+      var replyAction = $(this).find(".action-reply-container").first(),  // guard against embedded copy ("permalink-tweet" vs "original-tweet") on individual tweet page
+          classicRetweetAction = replyAction.clone(),
+          link = classicRetweetAction.find(".js-action-reply"),
+          label = "RT";    
+      classicRetweetAction.removeClass('action-reply-container');
+      classicRetweetAction.addClass('action-classicrt-container');
       link.removeClass("js-action-reply");
+      link.addClass('js-action-classicrt');
       link.removeAttr("data-modal");
-      var label = "RT";
       link.attr("title", label);
       link.find("b").text(label);
       link.find("span").attr("class", "icon sm-rt");
       replyAction.after(classicRetweetAction);
-
       link.on("click", function(event) {
-        var tweet = $(this).closest(".tweet");
-        var text = tweet.find(".js-tweet-text").first(); // guard as above
-        var replyTo = tweet.data('tweetId'); //get tweet ID for in-reply to
-        text.find("a").each(function(index) {
+        var tweet = $(this).closest(".tweet"),
+            text = tweet.find(".js-tweet-text").first().clone(),          // guard as above
+            replyLink = $(tweet).find(".action-reply-container .js-action-reply"),
+            title = "RT @" + tweet.data("screen-name"), 
+            content = "RT @" + tweet.data("screen-name") + ": " + text.text().trim();
+        text.find("a").each(function() {
           $(this).text($(this).data("expanded-url"));
         });
-        var title = "Classic Retweet", content = "RT @" + tweet.data("screen-name") + ": " + text.text().trim();
-        var dialog = $("#global-tweet-dialog");
-        // if we have the new tweet dialog, use that
-        if (dialog.length) {
-          $("#global-new-tweet-button").trigger("click");
-          dialog.find("h3").text(title);
-          dialog.find("#tweet-box-global").text(content).focus();
-          dialog.find("textarea.tweet-box-shadow").val(content);
-          dialog.find(".in-reply-to-status-id").val(replyTo); //maintain reply chain
-          // placeCaretAtEnd(dialog.find("#tweet-box-global").get(0));
-        }
-        // else use the old one
-        else if ($(".twttr-dialog-wrapper").length) {
-          // if we have direct access to page vars (e.g. chrome and firefox), open the dialog directly
-          if (window.twttr && twttr.widget && twttr.widget.TweetDialog) {
-            new twttr.widget.TweetDialog({
-              basic: false,
-              modal: false,
-              draggable: true,
-              template: {
-                title: title
-              },
-              defaultContent: content,
-              origin: "new-tweet-titlebar-button"
-            }).open().focus();
-          }
-        }
+        //Populate either in-line reply or modal window reply with old-school RT content
+        $(document).one("uiPrepareTweetBox", function (uiEvent) {
+          var replyForm = $(uiEvent.target),
+              replyTextBox = replyForm.find(".rich-editor");
+          $("#global-tweet-dialog").find("h3").text(title);               //only has effect if modal
+          replyTextBox.text(content).focus();
+          replyForm.find("textarea.tweet-box-shadow").val(content);
+        });
+        replyLink.click();
         event.preventDefault();
         event.stopPropagation();
         return false;
